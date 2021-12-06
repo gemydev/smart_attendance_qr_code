0bddab1c0417a8cc8f7e4ab784f2c77e27bd5d53
gemydev <mohamed_gamal1146@yahoo.com>
2021-12-06 18:27:27 +0200

    add QR example screen

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'package:smart_attendance_qr_code/functions/navigation_funs.dart';
+import 'package:smart_attendance_qr_code/functions/responsive_ui/app_info.dart';
+
+import 'capture_qr.dart';
 
 class HomePage extends StatefulWidget {
   HomePage({Key? key}) : super(key: key);
@@ -8,38 +13,42 @@ class HomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<HomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
+    AppInfo appInfo = AppInfo(context);
     return Scaffold(
       appBar: AppBar(
-        title: Text("الصفحة الرئيسية"),
+        title: Text("العفاني"),
+        centerTitle: true,
       ),
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+        child: GridView.builder(
+          itemCount: 3,
+          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 2,
+            childAspectRatio: 0.8,
+          ),
+          itemBuilder: (context, index) {
+            return Padding(
+              padding: const EdgeInsets.fromLTRB(10, 20, 10, 0),
+              child: Container(
+                width: (appInfo.screenWidth() - 20) * 0.3,
+                decoration: BoxDecoration(
+                    color: Colors.blueGrey,
+                    borderRadius: BorderRadius.all(Radius.circular(20)),
+                    border: Border.all(color: Colors.black, width: 2)),
+                child: GestureDetector(
+                  onTap: ()=> normalShift(context, QRViewExample()),
+                  child: Center(
+                      child: Text(
+                    "الصف الاول",
+                    style: TextStyle(color: Colors.white),
+                  )),
+                ),
+              ),
+            );
+          },
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
       ),
     );
   }
